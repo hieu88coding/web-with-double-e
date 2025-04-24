48cdb678ece434c32efb719550005f5ba348d3fd
Hiếu Cao <91723664+hieu88coding@users.noreply.github.com>
2025-04-24 09:59:12 +0700

    feat: add husky and lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  animate,
+  createScope,
+  createSpring,
+  createDraggable,
+  Scope,
+} from "animejs";
+import { useEffect, useRef, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const root = useRef(null);
+  const scope = useRef<Scope>(null);
+  const [rotations, setRotations] = useState(0);
+
+  useEffect(() => {
+    scope.current = createScope({ root }).add((scope) => {
+      // Every anime.js instances declared here are now scopped to <div ref={root}>
+
+      // Created a bounce animation loop
+      animate(".logo", {
+        scale: [
+          { to: 1.25, ease: "inOut(3)", duration: 200 },
+          { to: 1, ease: createSpring({ stiffness: 300 }) },
+        ],
+        loop: true,
+        loopDelay: 250,
+      });
+
+      // Make the logo draggable around its center
+      createDraggable(".logo", {
+        container: [0, 0, 0, 0],
+        releaseEase: createSpring({ stiffness: 200 }),
+      });
+
+      // Register function methods to be used outside the useEffect
+      scope.add("rotateLogo", (i) => {
+        animate(".logo", {
+          rotate: i * 360,
+          ease: "out(4)",
+          duration: 1500,
+        });
+      });
+    });
+
+    // Properly cleanup all anime.js instances declared inside the scope
+    return () => scope?.current?.revert();
+  }, []);
+
+  const handleClick = () => {
+    const i = rotations + 1;
+    setRotations(i);
+    // Animate logo rotation on click using the method declared inside the scope
+    if (scope.current) scope.current.methods.rotateLogo(i);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div ref={root}>
+      <div className="large centered row">
+        <img src={reactLogo} className="logo react" alt="React logo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="medium row">
+        <fieldset className="controls">
+          <button onClick={handleClick}>rotations: {rotations}</button>
+        </fieldset>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
